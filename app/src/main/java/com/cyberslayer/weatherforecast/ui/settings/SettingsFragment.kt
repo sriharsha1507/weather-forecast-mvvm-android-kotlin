f257774d1280b7d22220abcb7fc75ca579fc079f
sriharsha1507 <sriharsha1507@gmail.com>
2019-02-26 21:51:24 -0600

    * Added all necessary packages for the application

---
+++ app/src/main/java/com/cyberslayer/weatherforecast/ui/settings/SettingsFragment.kt
@@ -0,0 +1,7 @@
+package com.cyberslayer.weatherforecast.ui.settings
+
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment() {
+    //TODO Will change this to Preference Fragment Compat :)
+}
