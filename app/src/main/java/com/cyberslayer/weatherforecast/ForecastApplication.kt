d1a7cc5d1ef7138030cb998e4f19ae945d222ebb
sriharsha1507 <sriharsha1507@gmail.com>
2019-03-02 14:45:26 -0600

    Did Dependency Injection setup in Application class using Kodein library

---
+++ app/src/main/java/com/cyberslayer/weatherforecast/ForecastApplication.kt
@@ -0,0 +1,32 @@
+package com.cyberslayer.weatherforecast
+
+import android.app.Application
+import com.cyberslayer.weatherforecast.data.db.ForecastDatabase
+import com.cyberslayer.weatherforecast.data.network.*
+import com.cyberslayer.weatherforecast.data.repository.ForecastRepository
+import com.cyberslayer.weatherforecast.data.repository.ForecastRepositoryImpl
+import com.jakewharton.threetenabp.AndroidThreeTen
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.singleton
+
+class ForecastApplication : Application(), KodeinAware {
+    override val kodein: Kodein = Kodein.lazy {
+        import(androidXModule(this@ForecastApplication))
+
+        bind() from singleton { ForecastDatabase(instance()) }
+        bind() from singleton { instance<ForecastDatabase>().currentWeatherDao() }
+        bind<ConnectivityInterceptor>() with singleton { ConnectivityInterceptorImpl(instance()) }
+        bind() from singleton { ApixuWeatherApiService(instance()) }
+        bind<WeatherNetworkDataSource>() with singleton { WeatherNetworkDataSourceImpl(instance()) }
+        bind<ForecastRepository>() with singleton { ForecastRepositoryImpl(instance(), instance()) }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        AndroidThreeTen.init(this)
+    }
+}
