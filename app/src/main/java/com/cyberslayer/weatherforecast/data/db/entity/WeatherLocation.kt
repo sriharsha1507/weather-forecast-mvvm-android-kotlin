a512de15fe24fe1cd3ab3150949e1a3b62647b0a
sriharsha1507 <sriharsha1507@gmail.com>
2019-03-03 13:57:28 -0600

    Added WeatherLocation Entity

--- app/src/main/java/com/cyberslayer/weatherforecast/data/db/entity/WeatherLocation.kt
+++ app/src/main/java/com/cyberslayer/weatherforecast/data/db/entity/WeatherLocation.kt
@@ -1,7 +1,15 @@
 package com.cyberslayer.weatherforecast.data.db.entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+import org.threeten.bp.Instant
+import org.threeten.bp.ZoneId
+import org.threeten.bp.ZonedDateTime
 
+const val WEATHER_LOCATION_ID = 0
+
+@Entity(tableName = "weather_location")
 data class WeatherLocation(
     val name: String,
     val region: String,
@@ -11,6 +19,15 @@ data class WeatherLocation(
     @SerializedName("tz_id")
     val tzId: String,
     @SerializedName("localtime_epoch")
-    val localtimeEpoch: Int,
-    val localtime: String
-)
+    val localtimeEpoch: Long
+) {
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = WEATHER_LOCATION_ID
+
+    val zonedDateTime: ZonedDateTime
+        get() {
+            val instant = Instant.ofEpochSecond(localtimeEpoch)
+            val zoneId = ZoneId.of(tzId)
+            return ZonedDateTime.ofInstant(instant, zoneId)
+        }
+}
